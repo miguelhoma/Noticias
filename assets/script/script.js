2e27269272647b5e1fb0aa38bbfc2e5e47e0ea1a
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-02 18:40:45 +0200

    beta json innerhtml harsh

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,9 +2,8 @@ noticias_json = (noticias);
 
 console.log(noticias_json);
 
-lista = "";
-for (let i=0; i<noticias_json.length; i++) {
-    lista += "<p>"+noticias_json[i].titulo+"</p>";
-}
 
-document.getElementsByClassName("newsTitle")[0].innerHTML = lista;
+
+document.getElementsByClassName("newsTitle")[0].innerHTML = noticias_json[0]["titulo"];
+document.getElementsByClassName("newsSubtitle")[0].innerHTML = noticias_json[0]["subtitulo"];
+document.getElementsByClassName("newsImg")[0].src = noticias_json[0]["imagenUrl"];
