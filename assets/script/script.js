8c669ccd60d66fe6dc611118f509b3cb3e94d4ad
MatiasZito <matiaszito@gmail.com>
2022-06-02 18:16:20 +0200

    se realiza json

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,9 @@ noticias_json = (noticias);
 
 console.log(noticias_json);
 
+lista = "";
+for (let i=0; i<noticias_json.length; i++) {
+    lista += "<p>"+noticias_json[i].titulo+"</p>";
+}
+
+document.getElementsByClassName("newsTitle")[0].innerHTML = lista;
