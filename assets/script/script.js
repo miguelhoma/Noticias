b578d39f53cd7e3dd17ae99c088ab64a54a46aa2
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-03 16:52:30 +0200

    Merge branch 'main' of https://github.com/miguelhoma/Noticias

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,3 +23,40 @@ categoriaDeporte();
     newsImg[nA]           noticias_json[nA]  
 
 */
+
+// function para categoría de actualidad, en prueba con economía
+// function categoriaActualidad(){
+
+//     let nA = -1; 
+
+//     while(nA < 6){
+
+//         nA ++;
+//         let nB = 7;
+
+//             while ( nB < 14) {   
+
+//             document.getElementsByClassName("newsTitle Act")[nA].innerHTML = noticias_json[nB]["titulo"];
+//             document.getElementsByClassName("newsSubtitle Act")[nA].innerHTML = noticias_json[nB]["subtitulo"];
+//             document.getElementsByClassName("newsImg Act")[nA].src = noticias_json[nB]["imagenUrl"];
+//             document.getElementsByClassName("tit Act")[0].innerHTML = noticias_json[7]["categoria"];
+//             nB++;
+            
+//             }
+    
+//     }
+
+// }
+function categoryEconomy(){
+
+    for (let nC = 0, nB = 7; nC < 7 & nB < 14; nC++, nB++){
+        document.getElementsByClassName("newsTitle Actualidad")[nC].innerHTML = noticias_json[nB]["titulo"];
+        document.getElementsByClassName("newsSubtitle Actualidad")[nC].innerHTML = noticias_json[nB]["subtitulo"];
+        document.getElementsByClassName("newsImg Actualidad")[nC].src = noticias_json[nB]["imagenUrl"];
+        document.getElementsByClassName("tit Acttualidad")[0].innerHTML = noticias_json[7]["categoria"];
+
+}
+
+}
+
+categoryEconomy();
