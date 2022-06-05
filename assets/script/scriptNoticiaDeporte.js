3026301ffb7b8437a3dcad33b31b2282ed3f8dbd
MatiasZito <matiaszito@gmail.com>
2022-06-05 14:40:52 +0200

    se agrega referencias

--- assets/script/scriptNoticiaDeporte.js
+++ assets/script/scriptNoticiaDeporte.js
@@ -6,7 +6,7 @@ console.log(noticias_json);
 function categoriaDeporte(){
 
      for (let nA = 0; nA < 7; nA++) {
-        document.getElementsByClassName("tit")[nA].innerHTML = noticias_json[nA]["titulo"];
+        document.getElementsByClassName("tittle")[nA].innerHTML = noticias_json[nA]["titulo"];
         document.getElementsByClassName("subtit")[nA].innerHTML = noticias_json[nA]["subtitulo"];
         document.getElementsByClassName("imgenContenido")[nA].src = noticias_json[nA]["imagenUrl"];
         document.getElementsByClassName("cont")[nA].innerHTML = noticias_json[nA]["noticiap1"];
