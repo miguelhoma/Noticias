a19c0dd377d78395bc18c3ca3bc5745636430642
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-06 21:52:27 +0200

    test categoria sitio deseado

--- assets/script/catScriptHS.js
+++ assets/script/catScriptHS.js
@@ -1,27 +1,29 @@
 
 noticias_json = (noticias);
 
-function categoryAll(){
+function createCat(){
  
     contentAll = "";
     for (let bB = 0; bB < 112; bB++){
-        contentAll += '<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">titulo</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';
+        contentAll += '<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';
     }
+
     document.getElementsByClassName("categoryAll")[0].innerHTML = contentAll;
+
+    function fillCat(){
+        for (let nA = 0; nA < noticias_json.length; nA++) {
+            document.getElementsByClassName("newsTitle")[nA].innerHTML = noticias_json[nA]["titulo"];
+            document.getElementsByClassName("newsSubtitle")[nA].innerHTML = noticias_json[nA]["subtitulo"];
+            document.getElementsByClassName("newsImg")[nA].src = noticias_json[nA]["imagenUrl"];
+            document.getElementsByClassName("catName")[nA].innerHTML = noticias_json[nA]["categoria"];
+        }
+    }
+
+    fillCat();
 }
 
-categoryAll();
+createCat();
 
-
-function fillCat(){
-    for (let nA = 0; nA < noticias_json.length; nA++) {
-        document.getElementsByClassName("newsTitle")[nA].innerHTML = noticias_json[nA]["titulo"];
-        document.getElementsByClassName("newsSubtitle")[nA].innerHTML = noticias_json[nA]["subtitulo"];
-        document.getElementsByClassName("newsImg")[nA].src = noticias_json[nA]["imagenUrl"];
-        document.getElementsByClassName("catName")[nA].innerHTML = noticias_json[nA]["categoria"];
-}
-}
-fillCat();
 
 
 
@@ -43,3 +45,48 @@ fillCat();
 // testNewsNumbr();
 
 /*---------Acaba función del test----------------*/
+
+
+
+/*---------Test función para ver si cada noticia está en su lugar-------*/
+
+// function rightPlace(){
+
+//     for(tT = 0; tT <7; tT++){
+//         if (document.getElementsByClassName("catName")[tT].textContent.includes("Deporte")) {
+//         console.log("Cat dep correct");
+//         } else {
+//         console.log("cat dep incorrect");
+//         }
+//     }
+
+//     for(tT = 7; tT <14; tT++){
+//         if (document.getElementsByClassName("catName")[tT].textContent.includes("Economía")) {
+//         console.log("Cat eco correct");
+//         } else {
+//         console.log("cat eco incorrect");
+//         }
+//     }
+
+//     for(tT = 14; tT <21; tT++){
+//         if (document.getElementsByClassName("catName")[tT].textContent.includes("entretenimiento")) {
+//         console.log("Cat entre correct");
+//         } else {
+//         console.log("cat entre incorrect");
+//         }
+//     }
+
+//     for(tT = 21; tT <28; tT++){
+//         if (document.getElementsByClassName("catName")[tT].textContent.includes("politica")) {
+//         console.log("Cat poli correct");
+//         } else {
+//         console.log("cat poli incorrect");
+//         }
+//     }
+
+// }
+
+// rightPlace();
+
+
+/*---------Test función para ver si cada noticia está en su lugar acaba-------*/
