8e6fc271d0f4a7a8b4724306fc6b079560c25e76
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-08 00:27:14 +0200

    posible página de categorias (no version final)

--- assets/script/catScriptHS.js
+++ assets/script/catScriptHS.js
@@ -6,7 +6,23 @@ function createCat(){
     contentAll = "";
 
     for (let bB = 0; bB < 112; bB++){
+        if (bB == 0){
+            contentAll +='<div class="tit" id="deportes">Deportes <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        }
+        if (bB == 6){
+            contentAll +='<div class="tit" id="economia">Economía <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        }
+        if (bB == 12){
+            contentAll +='<div class="tit" id="enter">Entretenimiento <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        }
+        if (bB == 18){
+            contentAll +='<div class="tit" id="politica">Política <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        }
+        if (bB == 24){
+            contentAll +='<div class="tit" id="techno">Tecnología <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        }
         contentAll += '<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';
+        
     }
 
     document.getElementsByClassName("categoryAll")[0].innerHTML = contentAll;
