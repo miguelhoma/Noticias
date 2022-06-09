59c02abbd4672b6e22075b0d2c5afd6cc1dee44e
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-09 14:42:00 +0200

    update json 2

--- assets/script/catScriptHS.js
+++ assets/script/catScriptHS.js
@@ -5,22 +5,22 @@ function createCat(){
  
     contentAll = "";
 
-    for (let bB = 0; bB < 112; bB++){
-        if (bB == 0){
-            contentAll +='<div class="tit" id="deportes">Deportes <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
-        }
-        if (bB == 6){
-            contentAll +='<div class="tit" id="economia">Economía <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
-        }
-        if (bB == 12){
-            contentAll +='<div class="tit" id="enter">Entretenimiento <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
-        }
-        if (bB == 18){
-            contentAll +='<div class="tit" id="politica">Política <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
-        }
-        if (bB == 24){
-            contentAll +='<div class="tit" id="techno">Tecnología <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
-        }
+    for (let bB = 0; bB < noticias_json.length; bB++){
+        // if (bB == 0){
+        //     contentAll +='<div class="tit" id="deportes">Deportes <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        // }
+        // if (bB == 6){
+        //     contentAll +='<div class="tit" id="economia">Economía <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        // }
+        // if (bB == 12){
+        //     contentAll +='<div class="tit" id="enter">Entretenimiento <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        // }
+        // if (bB == 18){
+        //     contentAll +='<div class="tit" id="politica">Política <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        // }
+        // if (bB == 24){
+        //     contentAll +='<div class="tit" id="techno">Tecnología <a href=#backHome><span><img class="imagenLogoIcon" src="/assets/img/globeLogoIcon.jpeg" alt="Go on Top"></span></a></div>'+'<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';        
+        // }
         contentAll += '<div class="imagencConCategory"><img class="newsImg" src="" alt=""><div class="titulos"><div class="catName">categoría</div><div class="newsTitle">titulo</div><div class="newsSubtitle">subtiulo</div></div></div>';
         
     }
