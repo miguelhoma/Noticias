5d3d617615b35ffa6b6794566ce848120b454091
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-06-15 22:40:13 +0200

    función que muestra una noticias en la página del mismo index

--- assets/script/index.js
+++ assets/script/index.js
@@ -22,37 +22,13 @@ function sort(categoria){
 	}
 }
 
-
-function firt_display(){
-	console.log("hoa")
-	categoria =GetParam();
-	sort(categoria);
-	display_data(categoria, true)
-	let a="index.html?id="+categoria;
-	document.getElementById("ahref").href =a;
-
-}
-
-sort("Política")
-function on_button_clicked(){
-	categoria =GetParam();
-	//let categoria = document.getElementById("ver_mas").type;
-	document.getElementById("ver_mas").hidden = true;
-	display_data(categoria,false)
-	var flag=false;
-	let i = map.get(categoria);
-	let amount=6;
-	while ( i < noticias_json.length && amount<7) {
-		if (noticias_json[i]["categoria"] == categoria){
-			flag=true;
-			amount++;
-		}
-		i++
-
-	}
-	if (flag == true){add_button()}
-	
-
+function GetParam(){
+	let url = window.location.href
+	let temp = url.split("=")
+	let answer=temp[1];
+	if(answer.includes('%C3%AD')){answer=answer.replace('%C3%AD', 'í')}
+	if(answer.includes('%C3%B3')){answer=answer.replace('%C3%B3', 'ó')}
+	return answer
 }
 
 function display_data(categoria, flag){
@@ -81,12 +57,156 @@ function news_to_HTML(news,index){
 	let answer = '<div class="imagencConCategory">\
 					<img class="newsImg politica" src="'+news["imagenUrl"]+'" alt="'+news["alt"]+'">\
 					<div class="titulos">\
-						<div class="newsTitle politica"> <a href="index.html?id='+index+'"  >'+news["titulo"]+'</a></div>\
+						 <a href="index.html?id='+index+'" onclick="secondisplay()"><div class="newsTitle politica">'+news["titulo"]+'</a></div>\
 						<div class="newsSubtitle politica">'+news["subtitulo"]+'</div>\
 				    </div>\
 				 </div>';
 	return answer
 }
+
+function firt_display(){
+	console.log("hoa");
+	categoria =GetParam();
+	sort(categoria);
+	display_data(categoria, true);
+	let a = "index.html?id=" + categoria;
+	document.getElementById("ahref").href = a;
+
+}
+
+
+/* función para mostrar noticia con click desde la página de categoria */
+function GetParamNot(){
+	let url = window.location.href;
+	console.log(url);
+	let temp = url.split("=");
+	console.log(temp);
+	let answer = temp[1];
+	console.log(answer);
+	return answer;
+}
+
+
+function newsPage_to_html(news){
+    newsPage = '<section class="caja">\
+					<div class="tittle">'+news["titulo"]+'</div>\
+					<div class="subtit">'+news["subtitulo"]+'</div>\
+					<div class="lineaUno"></div>\
+					<div class="imagencont">\
+						<img class="imagenContenido" src="'+news["imagenUrl"]+'" alt="'+news["alt"]+'">\
+					</div>\
+					<div class="lineaDos"></div>\
+					<div class="contenidoTotal">\
+						<div class="fecha">'+news["fecha"]+'<div class = "autor">'+news["autor"]+'</div></div>\
+						<div class="cont">'+news["noticiap1"]+'</div>\
+						<div class="fuente">\
+							<a href="'+news["url noticia"]+' target="_blank"><div class="cont3">Origen</div></a>\
+						</div>\
+				</section>';
+
+    return newsPage;
+}
+
+function displayNewsData(noticiasIndex){
+
+    let noticiaHtml = '';
+	i = noticiasIndex - 1;
+    noticiaHtml += newsPage_to_html(noticias_json[i]);
+    document.querySelector("content").innerHTML = noticiaHtml;
+
+}
+
+function secondisplay(){
+    noticiasIndex = GetParamNot();
+    displayNewsData(noticiasIndex);
+	console.log("noticia" + noticiasIndex)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function on_button_clicked(){
+	categoria =GetParam();
+	//let categoria = document.getElementById("ver_mas").type;
+	document.getElementById("ver_mas").hidden = true;
+	display_data(categoria,false)
+	var flag=false;
+	let i = map.get(categoria);
+	let amount=6;
+	while ( i < noticias_json.length && amount<7) {
+		if (noticias_json[i]["categoria"] == categoria){
+			flag=true;
+			amount++;
+		}
+		i++
+
+	}
+	if (flag == true){add_button()}
+	
+
+}
+
 
 function add_button(){
 	let answer = '<div class="button">\
@@ -96,14 +216,6 @@ function add_button(){
 
 }
 
-function GetParam(){
-	let url = window.location.href
-	let temp = url.split("=")
-	let answer=temp[1];
-	if(answer.includes('%C3%AD')){answer=answer.replace('%C3%AD', 'í')}
-	if(answer.includes('%C3%B3')){answer=answer.replace('%C3%B3', 'ó')}
-	return answer
-}
 
 
 
